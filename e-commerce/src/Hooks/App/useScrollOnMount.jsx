9bd24c1628b14c14fbeb3659a265eb3338738295
niraj1703 <sharmaniraj76320@gmail.com>
2025-06-25 00:43:23 +0530

    commite

--- e-commerce/src/Hooks/App/useScrollOnMount.jsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-const useScrollOnMount = (top = 0, left = 0, behavior = "instant") => {
-  useEffect(() => {
-    window.scrollTo({ top, left, behavior });
-  }, []);
-};
-export default useScrollOnMount;
